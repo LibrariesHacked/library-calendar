cd0f49312e4fb22a938655f369d5c3598f97c699
Dave <8709533+DaveBathnes@users.noreply.github.com>
2022-03-31 22:10:49 +0100

    Adding calendar

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,17 @@ import { createTheme } from '@mui/material/styles'
 import { lightBlue, blueGrey } from '@mui/material/colors'
 
 const theme = createTheme({
+  palette: {
+    primary: {
+      main: lightBlue[800]
+    },
+    secondary: {
+      main: blueGrey[600]
+    },
+    outline: {
+      main: blueGrey[50]
+    }
+  },
   components: {
     MuiButton: {
       styleOverrides: {
@@ -10,6 +21,20 @@ const theme = createTheme({
           textTransform: 'none'
         },
         text: {
+          textTransform: 'none'
+        }
+      }
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none'
+        }
+      }
+    },
+    MuiTab: {
+      styleOverrides: {
+        root: {
           textTransform: 'none'
         }
       }
